0e1e63338ca00c7c1d50f1a3de7f7192f93c381a
Arter Tendean <arter.tendean.07@gmail.com>
2022-08-21 01:52:25 +0800

    update settings

--- simple_chat/settings.py
+++ simple_chat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!6j&obwj-n+2*y!#5(ly907*d)5egrsw@xdapv^$okvmi#=sn5"
+SECRET_KEY = "django-insecure-!6j&obwj-n+2*y!#5(ly907*d)5egrsw@xdapv^$okvmi#=sn5"  # opsss.... 🤣🙃😜😝
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
