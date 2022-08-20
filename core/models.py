c47d56dc91aff792c80f2d436d0c9dfb34629ae4
Arter Tendean <arter.tendean.07@gmail.com>
2022-08-21 01:51:37 +0800

    finished core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    room_name = models.CharField(max_length=50)
+    user = models.CharField(max_length=50)
+    message = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.room_name
--- core/views.py
+++ core/views.py
@@ -1,3 +1,77 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Chat
+
+
+@api_view(["GET", "POST"])
+def index(request):
+    if request.method == "POST":
+        # get POST data
+        room_name = request.POST.get("room_name", None)
+        user_name = request.POST.get("user_name", None)
+        user_message = request.POST.get("user_message", None)
+
+        # handle invalid POST data
+        if room_name == None or user_name == None or user_message == None:
+            response = {
+                "status_code": 400,
+                "message": "Invalid POST data.",
+                "data": {},
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+        # process the request
+        chat = Chat(room_name=room_name, user=user_name, message=user_message)
+        chat.save()  # save the chat to database
+
+        response = {
+            "status_code": 200,
+            "message": "Success.",
+            "data": {},
+        }
+        return Response(response, status=status.HTTP_201_CREATED)
+
+    if request.method == "GET":
+        room_name = request.GET.get("room_name", None)
+
+        # handle invalid room_name
+        if room_name == None:
+            response = {
+                "status_code": 404,
+                "message": "room_name cannot  be empty.",
+                "data": {},
+            }
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+
+        # get all chat from database
+        chats = Chat.objects.filter(room_name=room_name).order_by("-id")
+
+        if chats.exists() == False:
+            response = {
+                "status_code": 404,
+                "message": "Invalid Room Name.",
+                "data": {},
+            }
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+
+        _chat = []
+        for chat in chats:
+            _chat.append(
+                {
+                    "user": chat.user,
+                    "message": chat.message,
+                    "created_at": chat.created_at.timestamp(),
+                }
+            )
+
+        response = {
+            "status_code": 200,
+            "message": "Success.",
+            "data": {"room_name": room_name, "chats": _chat},
+        }
+        return Response(response)
+
+    return Response({})
